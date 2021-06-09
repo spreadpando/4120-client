2000a32954dc76fdb7909802a606a040bd418260
pando <spreadpando@gmail.com>
2021-06-09 16:51:04 -0600

    Updates

---
+++ precache-manifest.7be73e8306d28434b53d227178acaff4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e90fe6b31f0d67e86a4af199e878ed",
+    "url": "/4120-client/index.html"
+  },
+  {
+    "revision": "e4f3c829bb6587327aea",
+    "url": "/4120-client/static/css/main.2ac4f1ff.chunk.css"
+  },
+  {
+    "revision": "2737b91d39dbba803aaf",
+    "url": "/4120-client/static/js/2.9ed90000.chunk.js"
+  },
+  {
+    "revision": "e4f3c829bb6587327aea",
+    "url": "/4120-client/static/js/main.703afe66.chunk.js"
+  },
+  {
+    "revision": "baddbfbf4d55465e1229",
+    "url": "/4120-client/static/js/runtime-main.edec9d37.js"
+  },
+  {
+    "revision": "740bfe7cfc8340da936939e3b0fb605a",
+    "url": "/4120-client/static/media/4120-logo-y.740bfe7c.svg"
+  },
+  {
+    "revision": "77a632b94766e3275511aee6f9a80c84",
+    "url": "/4120-client/static/media/desert-sky.77a632b9.png"
+  },
+  {
+    "revision": "29bf261c64c967a8d7f199fa40d1a518",
+    "url": "/4120-client/static/media/down.29bf261c.svg"
+  },
+  {
+    "revision": "88de0df4543aff3a389c1d2d4a736bc3",
+    "url": "/4120-client/static/media/ground.88de0df4.jpg"
+  },
+  {
+    "revision": "92a28d9a0c29bfffe26b6e9a84b8d8ca",
+    "url": "/4120-client/static/media/left.92a28d9a.svg"
+  },
+  {
+    "revision": "593ccdac4c7d0429700b0b169ae16e9d",
+    "url": "/4120-client/static/media/right.593ccdac.svg"
+  },
+  {
+    "revision": "05d73c6fc1a460435e235dc4f2b04c7d",
+    "url": "/4120-client/static/media/up.05d73c6f.svg"
+  },
+  {
+    "revision": "f848685e65d8ebdcc679cb69cf1f2044",
+    "url": "/4120-client/static/media/vr_logo.f848685e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/4120-client/precache-manifest.d59f4a90612db7d2b251c4ae1097e927.js"
+  "/4120-client/precache-manifest.7be73e8306d28434b53d227178acaff4.js"
 );
 
 self.addEventListener('message', (event) => {
