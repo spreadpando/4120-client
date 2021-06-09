4162c59425b9c23fae29bee90c8a7e13372aa725
pando <spreadpando@gmail.com>
2021-06-09 16:09:36 -0600

    Updates

---
+++ precache-manifest.d59f4a90612db7d2b251c4ae1097e927.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648eb9797cfd2041d6e5ff0ce447c75d",
+    "url": "/4120-client/index.html"
+  },
+  {
+    "revision": "ac21d8105f24ef1183ab",
+    "url": "/4120-client/static/css/main.2ac4f1ff.chunk.css"
+  },
+  {
+    "revision": "445b6fa5a458544c2337",
+    "url": "/4120-client/static/js/2.33b21d8f.chunk.js"
+  },
+  {
+    "revision": "ac21d8105f24ef1183ab",
+    "url": "/4120-client/static/js/main.c3a95ba0.chunk.js"
+  },
+  {
+    "revision": "3e9f2a999634f396831a",
+    "url": "/4120-client/static/js/runtime-main.c67d0a90.js"
+  },
+  {
+    "revision": "740bfe7cfc8340da936939e3b0fb605a",
+    "url": "/4120-client/static/media/4120-logo-y.740bfe7c.svg"
+  },
+  {
+    "revision": "77a632b94766e3275511aee6f9a80c84",
+    "url": "/4120-client/static/media/desert-sky.77a632b9.png"
+  },
+  {
+    "revision": "29bf261c64c967a8d7f199fa40d1a518",
+    "url": "/4120-client/static/media/down.29bf261c.svg"
+  },
+  {
+    "revision": "88de0df4543aff3a389c1d2d4a736bc3",
+    "url": "/4120-client/static/media/ground.88de0df4.jpg"
+  },
+  {
+    "revision": "92a28d9a0c29bfffe26b6e9a84b8d8ca",
+    "url": "/4120-client/static/media/left.92a28d9a.svg"
+  },
+  {
+    "revision": "593ccdac4c7d0429700b0b169ae16e9d",
+    "url": "/4120-client/static/media/right.593ccdac.svg"
+  },
+  {
+    "revision": "05d73c6fc1a460435e235dc4f2b04c7d",
+    "url": "/4120-client/static/media/up.05d73c6f.svg"
+  },
+  {
+    "revision": "f848685e65d8ebdcc679cb69cf1f2044",
+    "url": "/4120-client/static/media/vr_logo.f848685e.png"
+  }
+]);
